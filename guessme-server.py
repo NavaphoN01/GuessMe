550b9609e7bba39a43e5fb7dfec3e8a6dc80254a
Muhammadsofron Malee <6510110676@email.psu.ac.th>
2023-03-03 16:03:10 +0700

    define accept_wrapper

--- guessme-server.py
+++ guessme-server.py
@@ -1,4 +1,10 @@
 import random
+import socket
+import selectors
+import types
+
+HOST = "127.0.0.1"
+PORT = 65432
 
 class Game:
     def __init__(self):
@@ -14,3 +20,37 @@ class Game:
             return "Too low.\n"
         else:
             return "Too high.\n"
+
+sel = selectors.DefaultSelector()
+
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((HOST, PORT))
+lsock.listen()
+print(f"Listening on {(HOST, PORT)}")
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+socks = []
+
+def accept_wrapper(sock):
+    conn, addr = sock.accept()
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+    game = Game()
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"", game=game)
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+    socks.append(conn)
+
+try:
+    while True:
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                 accept_wrapper(key.fileobj)
+            else:
+                pass
+except KeyboardInterrupt:
+    print("Caught keyboard interrupt, exiting")
+finally:
+    sel.close()
